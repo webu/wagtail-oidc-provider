e024916418ae3f54e72be8efed4d21a242630fcc
Samuël WEBER <s.weber@webu.coop>
2025-05-13 15:10:41 +0200

    feat: update admin

--- wagtail_oidc_provider/wagtail_hooks.py
+++ wagtail_oidc_provider/wagtail_hooks.py
@@ -10,7 +10,9 @@ from oidc_provider.models import Code
 from oidc_provider.models import RSAKey
 from oidc_provider.models import Token
 from wagtail.admin.panels import FieldPanel
+from wagtail.admin.panels import HelpPanel
 from wagtail.admin.panels import MultiFieldPanel
+from wagtail.admin.panels import ObjectList
 from wagtail.snippets.models import register_snippet
 from wagtail.snippets.views.snippets import SnippetViewSet
 from wagtail.snippets.views.snippets import SnippetViewSetGroup
@@ -54,8 +56,9 @@ class ClientForm(ModelForm):
 
 class ClientViewSet(SnippetViewSet):
     model = Client
+    icon = "globe"
 
-    panels = [
+    client_panels = [
         MultiFieldPanel(
             children=[
                 FieldPanel("name"),
@@ -83,7 +86,7 @@ class ClientViewSet(SnippetViewSet):
                 FieldPanel("website_url"),
                 FieldPanel("terms_url"),
                 FieldPanel("logo"),
-                FieldPanel("date_created"),
+                FieldPanel("date_created", read_only=True),
             ],
             heading=_("Information"),
         ),
@@ -94,41 +97,41 @@ class ClientViewSet(SnippetViewSet):
             heading=_("Session Management"),
         ),
     ]
+    edit_handler = ObjectList(client_panels, base_form_class=ClientForm)
 
-    form = ClientForm
     list_display = ["name", "client_id", "response_type_descriptions", "date_created"]
-    readonly_fields = ["date_created"]
     search_fields = ["name"]
-    raw_id_fields = ["owner"]
 
 
 class CodeViewSet(SnippetViewSet):
     model = Code
+    icon = "cogs"
 
-    raw_id_fields = ["user"]
-
-    def has_add_permission(self, request):
-        return False
+    inspect_view_enabled = True
 
 
 class TokenViewSet(SnippetViewSet):
     model = Token
+    icon = "cogs"
 
-    raw_id_fields = ["user"]
-
-    def has_add_permission(self, request):
-        return False
+    inspect_view_enabled = True
 
 
 class RSAKeyViewSet(SnippetViewSet):
     model = RSAKey
+    icon = "key"
 
-    readonly_fields = ["kid"]
+    panels = [
+        FieldPanel("key"),
+        HelpPanel(
+            heading="Kid", template="wagtailadmin/wagtail_oidc_provider/panels/kid_panel.html"
+        ),
+    ]
 
 
 class OidcProviderGroup(SnippetViewSetGroup):
     menu_label = "OpenID connect provider"
-    menu_icon = "snippet"
+    menu_icon = "resubmit"
     menu_order = 120
     items = (ClientViewSet, CodeViewSet, TokenViewSet, RSAKeyViewSet)
 
